c43795a826083f730d7cef81141b3c40bb8b4bea
ayel-mou <ayel-mou@student.1337.ma>
2024-01-29 07:19:10 +0100

    update

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,35 +6,29 @@
 /*   By: ayel-mou <ayel-mou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 22:58:41 by ayel-mou          #+#    #+#             */
-/*   Updated: 2024/01/29 06:49:33 by ayel-mou         ###   ########.fr       */
+/*   Updated: 2024/01/29 07:18:23 by ayel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void free_char_array(char **arr)
+void	free_char_array(char **arr)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (arr[i] != NULL)
-    {
-        free(arr[i]);
-        i++;
-    }
-    free(arr);
+	i = 0;
+	while (arr[i] != NULL)
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
 }
 
-void free_parsing_result(char **par, char *tmp)
+int	ft_strlen_array(char **arr)
 {
-    free_char_array(par);
-    free(tmp);
-}
+	int	length;
 
-int ft_strlen_array(char **arr)
-{
-	int length;
-	
 	length = 0;
 	while (arr[length] != NULL)
 	{
@@ -71,26 +65,26 @@ char	**parsing(int ac, char **av)
 	return (nbrs);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	t_stack *a;
-	t_stack *b;
-	char **par;
+	t_stack	*a;
+	t_stack	*b;
+	char	**par;
 
 	a = NULL;
 	b = NULL;
 	if (ac < 2)
 		return (0);
-    par = parsing(ac, av);
+	par = parsing(ac, av);
 	ft_check_errors(par);
 	a = init_stack(ft_strlen_array(par), par);
 	free_char_array(par);
 	if (!is_sorted(a))
 	{
-		if (stack_size(a) == 2) 
+		if (stack_size(a) == 2)
 			sa(&a);
 	}
-    print_stack(a);
+	print_stack(a);
 	free_stack(a);
 	return (0);
 }
