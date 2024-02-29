7717730c19301816087a1632c6fb9b2f357fffb7
ayel-mou <ayel-mou@student.1337.ma>
2024-02-29 04:09:41 +0100

    update

--- test.sh
+++ test.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-# Define the number of blocks to display
+
 num_blocks=30
-
-# Define the delay between each frame (adjust as needed)
 delay=0.1
-
-# Function to display the loading animation
 loading_animation() {
     local progress
     for ((progress = 1; progress <= num_blocks; progress++)); do
         printf "\r"
-        printf "\e[38;5;154m█%.0s" $(seq 1 $progress)  # Lime color code: \e[38;5;154m
-        printf "\e[0m"  # Reset color
+        printf "\e[38;5;154m█%.0s" $(seq 1 $progress)
+        printf "\e[0m" 
         sleep $delay
     done
     printf "\n"
 }
-
-# Start the loading animation
 loading_animation
